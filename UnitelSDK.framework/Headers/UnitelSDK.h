7be5b6881c8452f159f7f5f5729f0e70a5b5e4bc
Phạm Trung Chính <chinhph97@gmail.com>
2022-08-16 10:50:25 +0700

    add new

---
+++ UnitelSDK.framework/Headers/UnitelSDK.h
@@ -0,0 +1,18 @@
+//
+//  UnitelSDK.h
+//  UnitelSDK
+//
+//  Created by Anh Loc Mascom on 01/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnitelSDK.
+FOUNDATION_EXPORT double UnitelSDKVersionNumber;
+
+//! Project version string for UnitelSDK.
+FOUNDATION_EXPORT const unsigned char UnitelSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnitelSDK/PublicHeader.h>
+
+
